b6eed74b3ea07327fffd1275d37b475c915dadc4
ndessai <ndessai@gmail.com>
2017-06-18 22:46:40 -0400

    adding anagram and space replacement

---
+++ ArraysAndStrings/Anagrams/Program.cs
@@ -0,0 +1,77 @@
+﻿//anagram check
+
+using System;
+
+namespace AnagramCheck
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var p = new Program();
+            string str1 = "working";
+            string str2 = "krogwin";
+            Console.WriteLine(str1 + " " + str2 + " " + p.IsAnagram(str1, str2));
+
+            str1 = "working";
+            str2 = "krogwin2";
+            Console.WriteLine(str1 + " " + str2 + " " + p.IsAnagram(str1, str2));
+
+            str1 = "working1";
+            str2 = "krogwin";
+            Console.WriteLine(str1 + " " + str2 + " " + p.IsAnagram(str1, str2));
+            Console.Read();
+        }
+
+        private bool IsAnagram(string s1, string s2)
+        {
+            if (string.IsNullOrWhiteSpace(s1) && string.IsNullOrWhiteSpace(s2))
+            {
+                return true;
+            }
+
+            if (string.IsNullOrWhiteSpace(s1) || string.IsNullOrWhiteSpace(s2))
+            {
+                return false;
+            }
+
+            if (s1.Length != s2.Length)
+            {
+                return false;
+            }
+
+            int[] charCount = new int[256];
+            for (int i = 0; i < 256; i++)
+            {
+                charCount[i] = 0;
+            }
+
+            int num_unique_chars = 0;
+            int num_unique_counted = 0;
+            foreach (char c in s1)
+            {
+                if (charCount[c] == 0)
+                {
+                    num_unique_chars++;
+                }
+                charCount[c]++;
+            }
+
+            for (int i = 0; i < s2.Length; i++)
+            {
+                char c = s2[i];
+                charCount[c]--;
+                if (charCount[c] == 0)
+                {
+                    num_unique_counted++;
+                    if (num_unique_chars == num_unique_counted)
+                    {
+                        return s2.Length - 1 == i;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ ArraysAndStrings/ReplaceSpace/Program.cs
@@ -0,0 +1,53 @@
+﻿//space (' ') replacement with "%20"
+
+using System;
+
+namespace ReplaceSpace
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var p = new Program();
+            string input = "This is a space string";
+            p.ReplaceSpaceChars(input);
+            Console.Read();
+        }
+
+        private void ReplaceSpaceChars(string input)
+        {
+            int numOfSpaces = 0;
+            foreach (var c in input)
+            {
+                if (c == ' ')
+                {
+                    numOfSpaces++;
+                }
+            }
+
+            int replacedLength = numOfSpaces * 2 + input.Length;
+            char[] replaced = new char[replacedLength];
+            if (numOfSpaces > 0)
+            {
+                for (int i = input.Length - 1; i >= 0; i--)
+                {
+                    if (input[i] == ' ')
+                    {
+                        replaced[replacedLength - 1] = '0';
+                        replaced[replacedLength - 2] = '2';
+                        replaced[replacedLength - 3] = '%';
+                        replacedLength -= 3;
+                    }
+                    else
+                    {
+                        replaced[replacedLength - 1] = input[i];
+                        replacedLength--;
+                    }
+                }
+            }
+            Console.WriteLine(input + "   => " + new String(replaced));
+        }
+    }
+
+}
