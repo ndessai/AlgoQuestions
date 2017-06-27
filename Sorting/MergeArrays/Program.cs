53e3833f2bae4bcf6888f6b8691948066960a27a
ndessai <ndessai@gmail.com>
2017-06-27 17:12:02 -0400

    adding string sorting using anagrams

---
+++ Sorting/MergeArrays/Program.cs
@@ -0,0 +1,41 @@
+﻿//quick sort
+
+using System;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        var p= new Program();
+        int[] A = new int[] { 1, 3, 5, 7, 9 };
+        int[] B = new int[9];
+
+        B[0] = 2; B[1] = 4; B[2] = 6; B[3] = 8;
+
+        p.MergeAIntoB(A, ref B, 4);
+        foreach (var i in B)
+        {
+            Console.Write(i + " ");
+        }
+        Console.Read();
+    }
+
+    private void MergeAIntoB(int[] A, ref int[] B, int bLength)
+    {
+        int total = A.Length + bLength - 1;
+
+        int a = A.Length - 1;
+        int b = bLength - 1;
+
+        while (a >= 0 && b >= 0)
+        {
+            if (A[a] > B[b])
+                B[total--] = A[a--];
+            else
+                B[total--] = B[b--];
+        }
+        if (a >= 0)
+            while (a >= 0) B[total--] = A[a--];
+    }
+}
---
+++ Sorting/StringSorting/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = new string[] { "his", "an", "na"};
+            Array.Sort(array1);
+            foreach (var i in array1)
+            {
+                Console.WriteLine(i);
+            }
+
+            Array.Sort(array1, new AnagramComparer());
+            foreach (var i in array1)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.Read();
+
+        }
+    }
+
+    public class AnagramComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            char[] xArray = x.ToArray();
+            char[] yArray = y.ToArray();
+            Array.Sort(xArray);
+            Array.Sort(yArray);
+            if(xArray.Length == yArray.Length)
+            {
+                int i = 0;
+                for(i = 0;i <xArray.Length; i++)
+                {
+                    if(xArray[i] != yArray[i])
+                    {
+                        break;
+                    }
+                }
+                int j = x.CompareTo(y);
+                if(i == xArray.Length)
+                {
+                    return -1;
+                }
+            }
+            return x.CompareTo(y);
+        }
+    }
+}
