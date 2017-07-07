097b120f46f97ffa89f54722212d1068ed9b4a3c
ndessai <ndessai@gmail.com>
2017-07-07 17:32:19 -0400

    adding bitwise

---
+++ Bitwise/OneInThrees/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] array = { 13, 14, 15, 6, 13, 15, 14, 2, 13, 2, 15, 14, 2 };
+        var p = new Program();
+        Console.WriteLine("Single is " + p.GetSingle(array));
+        Console.Read();
+    }
+
+    private int GetSingle(int[] array)
+    {
+        int ones = 0; int twos = 0;
+        int common_bit_mask = 0;
+
+        foreach (var no in array)
+        {
+            twos |= (ones & no);
+            ones ^= no;
+
+            common_bit_mask = ~(ones & twos);
+            ones &= common_bit_mask;
+            twos &= common_bit_mask;
+        }
+        return ones;
+    }
+}
---
+++ TreesAndGraphs/CommonGraph/GraphMatrixMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class GraphWithMatrix<T>
+{
+    T[] Nodes { get; set; }
+    int? [,] Costs  {get;set;}
+
+    public GraphWithMatrix(T [] nodeNames)
+    {
+        Nodes = nodeNames;
+        Costs = new int?[Nodes.Length, Nodes.Length];
+    }
+
+    public GraphWithMatrix(int?[,] costs)
+    {
+        Costs = costs;
+
+    }
+}
