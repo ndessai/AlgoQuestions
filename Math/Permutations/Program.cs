a48fc9cb5f14b6e2a3993c21d852a87e811b1401
ndessai <ndessai@gmail.com>
2017-07-11 12:09:23 -0400

    adding permutations

---
+++ Math/Permutations/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Permutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            foreach(var str in p.GetPermutations("This"))
+            {
+                Console.WriteLine(str);
+            }
+            Console.Read();
+        }
+
+        private List<string> GetPermutations(string str)
+        {
+            List<string> result = new List<string>();
+            if(str == null)
+            {
+                return null;
+            } else if(string.IsNullOrEmpty(str))
+            {
+                result.Add("");
+                return result;
+            }
+
+            char c = str[0];
+            string remainder = str.Substring(1);
+            var subPerms = GetPermutations(remainder);
+            foreach(var perm in subPerms)
+            {
+                var array = perm.ToCharArray();
+                for(int i = 0;i<=array.Length;i++)
+                {
+                    var newArray = new char[array.Length + 1];
+                    int k = 0;
+                    for(int m=0;m< newArray.Length; m++)
+                    {
+                        if(m==i) { newArray[m] = c;  continue; }
+
+                        newArray[m++] = array[k++];
+                    }
+                    result.Add(new string(newArray));
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Math/Subsets/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subsets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new Program().GetSubsets(new List<int>(new int[] { 1, 2, 3, 4, 5 }), 0);
+            foreach(var item in result)
+            {
+                Console.WriteLine();
+                foreach (var e in item) Console.Write(e + "  ");
+            }
+            Console.Read();
+
+        }
+
+        private List<List<int>> GetSubsets(List<int> set, int index)
+        {
+            List<List<int>> allsubSets;
+
+            if(set.Count == index)
+            {
+                allsubSets =  new List<List<int>>();
+                allsubSets.Add(new List<int>());
+            }
+            else
+            {
+                allsubSets = GetSubsets(set, index + 1);
+                int item = set[index];
+                var moreSubsets = new List<List<int>>();
+                foreach (var subset in allsubSets)
+                {
+                    var newSubset = new List<int>();
+                    newSubset.AddRange(subset);
+                    newSubset.Add(item);
+                    moreSubsets.Add(newSubset);
+                }
+                allsubSets.AddRange(moreSubsets);
+            }
+
+            return allsubSets;
+        }
+    }
+}
