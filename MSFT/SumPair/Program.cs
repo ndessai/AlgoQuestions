039e32007aafb07e654c04633e3332aea6f87970
ndessai <ndessai@gmail.com>
2017-08-10 09:12:47 -0400

    adding sum pair

---
+++ MSFT/SumPair/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class A
+{
+    public static void Main(string[] args)
+    {
+        int[] a = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        var p = new A();
+        p.PrintSumPair(a, 14);
+        Console.Read();
+    }
+
+    private void PrintSumPair(int[] a, int sum)
+    {
+        HashSet<int> hashSet = new HashSet<int>();
+        foreach (var item in a)
+        {
+            int diff = sum - item;
+            if (hashSet.Contains(diff)){
+                Console.WriteLine("Found " + item + " " + (sum - item));
+                return;
+            }
+            hashSet.Add(item);
+        }
+        Console.WriteLine("Not found");
+    }
+}
